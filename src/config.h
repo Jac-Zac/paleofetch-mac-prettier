35694c8bc60eeb5c285f8a5f74e600c497bf56a3
Jac-Zac <jacopozac@icloud.com>
2023-01-30 00:47:45 +0100

    changing icons

--- src/config.h
+++ src/config.h
@@ -3,21 +3,21 @@
     /* name            function                 cached  */\
 	{ "",             print_hardware,              false  }, \
 	SPACER \
-    { " \033[33m\033[0m    ",    get_machine,              true  }, \
-	{ " \033[33m󰻠\033[0m    ",        get_cpu,                  true  }, \
-	{ " \033[33m﬙\033[0m    ",        get_gpu,                  true  }, \
-	{ " \033[33m塞\033[0m   ", 		  get_ram_usage,		    false }, \
-    { " \033[33m\033[0m    ",    get_battery_procentage,   false }, \
+    { " \033[33m\033[0m    ",     get_machine,              true  }, \
+	{ " \033[33m󰻠\033[0m    ",     get_cpu,                  true  }, \
+	{ " \033[33m﬙\033[0m    ",     get_gpu,                  true  }, \
+	{ " \033[33m\033[0m    ",     get_ram_usage,		     false }, \
+    { " \033[33m\033[0m    ",     get_battery_procentage,   false }, \
 	SPACER \
 	{ "",             print_software,              false  }, \
 	SPACER \
-	{ " \033[33m\033[0m    ",		  get_complete_os,          true  }, \
-	{ " \033[33m\033[0m    ",     get_kernel,               true  }, \
-	{ " \033[33m\033[0m    ",   get_terminal,             false }, \
-	{ " \033[33m󱆃\033[0m    ",      get_shell,                true  }, \
+	{ " \033[33m\033[0m    ",	    get_complete_os,          true  }, \
+	{ " \033[33m\033[0m    ",     get_kernel,               true  }, \
+	{ " \033[33m\033[0m    ",     get_terminal,             false }, \
+	{ " \033[33m󱆃\033[0m    ",     get_shell,                true  }, \
 	{ " \033[33m󱦟\033[0m    ",     get_uptime,               false }, \
 	SPACER \
-	{ "",             print_end_info,              false  }, \
+	{ "",             print_end_info,               false  }, \
 	{ "",             get_colors_dots,              false  }, \
 	/* { "",             get_colors1,              true  }, \ */\
 	/* { "",             get_colors2,              true  }, \ */\
